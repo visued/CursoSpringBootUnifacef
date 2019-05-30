f30780778f51e342b3a0dcacf84b1c4ab445db5f
VICTOR VINICIUS SUED FLAVIO <21223@unifacef.edu.br>
2019-05-29 21:10:43 -0300

    Criado endpoint que recebe varios parametros

--- src/main/java/com/sued/Endpoints.java
+++ src/main/java/com/sued/Endpoints.java
@@ -1,5 +1,7 @@
 package com.sued;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,14 +18,19 @@ public class Endpoints {
 	}
 
 	@GetMapping("/teste/lol")
-	public String getHelloRoland(@RequestParam(name = "name") String championName, @RequestParam String race) {
+	public String getParam(@RequestParam(name = "name") String championName, @RequestParam String race) {
 		return "Name: " + championName + " Race:" + race;
 	}
 
 	@GetMapping("/lol")
-	public String getHelloRoland2(@RequestParam(name = "name") String championName,
+	public String getParam2(@RequestParam(name = "name") String championName,
 			@RequestParam(defaultValue = "não reconhecido") String race) {
 		return "Name: " + championName + " Race:" + race;
 	}
+	
+	@GetMapping("/lol/nparams")
+	public String getParamList(@RequestParam Map<String, String> parameters) {
+		return "Parâmetros " + parameters.entrySet();
+	}	
 
 }
