f021efa2d956b651b9ad88c733a3ddaef9d91174
VICTOR VINICIUS SUED FLAVIO <21223@unifacef.edu.br>
2019-05-29 21:47:44 -0300

    Criando enpoint com post

--- src/main/java/com/sued/Endpoints.java
+++ src/main/java/com/sued/Endpoints.java
@@ -3,8 +3,12 @@ package com.sued;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,5 +48,14 @@ public class Endpoints {
 	public String getParamPathVariable(@PathVariable(required = false) String name) {
 		return "Name:" + name;
 	}
+	
+	@PostMapping("professor")
+	@ResponseBody
+	public ResponseEntity postProfessor(@RequestBody Professor professor) {
+		//validando dados
+		//salvando no banco
+		return ResponseEntity.status(HttpStatus.OK).body(professor);
+	}
+	
 
 }
---
+++ src/main/java/com/sued/Professor.java
@@ -0,0 +1,19 @@
+package com.sued;
+
+public class Professor {
+	private String nome;
+	private String sobrenome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+}
